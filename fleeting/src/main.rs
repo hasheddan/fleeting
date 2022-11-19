d32c0206fe276cd0fc7d0dc9a9fb99eec4b93486
hasheddan <georgedanielmangum@gmail.com>
2022-11-19 17:28:23 -0500

    Add fleeting crate with axum as web framework
    Adds the initial fleeting crate, which uses axum as framework.
    
    Signed-off-by: hasheddan <georgedanielmangum@gmail.com>

---
+++ fleeting/src/main.rs
@@ -0,0 +1,21 @@
+use axum::{routing::get, Router};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new().route("/", get(root));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::debug!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+// basic handler that responds with a static string
+async fn root() -> &'static str {
+    "Welcome to fleeting!"
+}
